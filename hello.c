e3dae9024472dcb27e107281b45417bd01c4534e
smnumaan <smnumaan051@gmail.com>
2025-03-10 06:41:33 +0530

    old first project

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+printf("hello world!\n");
+
+
+int x = 100;
+
+if (x > 20)
+{ 
+    printf("x is greater than 20"); 
+}
+else if (x < 20)
+{
+    printf("x is less\n");
+}
+else 
+{
+    printf("x is 20\n");
+}
+
+return 0;
+
+}
+    
+
