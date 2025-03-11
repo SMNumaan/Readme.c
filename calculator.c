dec229a14351cd3bef9c296a40cccd9d6aa07469
smnumaan <smnumaan051@gmail.com>
2025-03-11 06:25:53 +0530

    added all operations in cal also learned string comparison like strcmp and using float for devision

--- calculator.c
+++ calculator.c
@@ -1,12 +1,30 @@
 #include<stdio.h>
- 
+ #include<string.h>
 int main()
 {
-    int n , m;
+    float n , m;
+    char str[10];
 
     printf("choose numbers: ");
-    scanf("%d %d", &n , &m);
-    printf("Sum: %d" , n + m );
+    scanf("%f %f", &n , &m);
+    
+    printf("what do u wanna do?");
+    scanf("%s" , &str);
+
+    if (strcmp(str, "sum") == 0)  // Use strcmp() for string comparison
+    printf("Sum: %f" , n + m );
+
+    else if (strcmp(str , "sub") == 0)
+    printf("Difference: %f" , n - m); 
+
+    else if (strcmp(str , "product") == 0)
+    printf("Product: %f" , n * m);
+
+    else if (strcmp(str , "devide") == 0)
+    printf("Quotient: %f" , n / m);
+
+    else 
+    printf("invalid operation\n");
 
     
     return 0;
