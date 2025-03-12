4097941c5bba45496da85968f93cca72d45992c7
smnumaan <smnumaan051@gmail.com>
2025-03-12 06:15:23 +0530

    claning code (completing)

--- calculator.c
+++ calculator.c
@@ -1,30 +1,35 @@
 #include<stdio.h>
  #include<string.h>
+
 int main()
 {
-    int n , m;
-    char str[10];
+    
+    int num1 , num2;
+    char str[20];
 
-    printf("choose numbers: ");
-    scanf("%d %d", &n , &m);
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d" , &num2);
     
     printf("what do u wanna do?");
     scanf("%s" , &str);
 
     if (strcmp(str, "sum") == 0)  // Use strcmp() for string comparison
-    printf("Sum: %d" , n + m );
+    printf("Sum: %d" , num1 + num2);
 
     else if (strcmp(str , "sub") == 0)
-    printf("Difference: %d" , n - m); 
+    printf("Difference: %d" , num1 - num2); 
 
     else if (strcmp(str , "product") == 0)
-    printf("Product: %d" , n * m);
+    printf("Product: %d" , num1 * num2);
 
-    else if (strcmp(str , "devide") == 0)
-    printf("Quotient: %.2f" , (float) n / m);
+    else if (strcmp(str , "division") == 0)
+    printf("Quotient: %.2f" , (float) num1 / num2); // using float specifically for proper division
 
     else 
-    printf("invalid operation\n");
+    printf("invalid operation\n"); // handling
 
     
     return 0;
